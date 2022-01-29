b8f172b9c4d89daee7f45db4c075bcadd28666f3
John Climie <johnclimie00@gmail.com>
2022-01-29 15:52:14 -0500

    Created connection const

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const inquierer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 
+// Creates SQL connection
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'tracker_db'
+});
+
