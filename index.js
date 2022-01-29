daf43bffbba7c49ec36f3f0186719c2f369d0070
John Climie <johnclimie00@gmail.com>
2022-01-29 16:11:06 -0500

    Added start prompts and viewDep function

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Required packages
-const inquierer = require('inquirer');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 
 // Creates SQL connection
-
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -12,3 +11,32 @@ const connection = mysql.createConnection({
     database: 'tracker_db'
 });
 
+// Initial prompt questions
+const start = () => {
+    inquirer
+        .prompt([
+            {
+                name:'start',
+                message: 'What would you like to do?',
+                type: 'list',
+                choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee']
+            }
+        ])
+}
+
+
+const viewDep = () => {
+    connection 
+        .query('SELECT * FROM department', function(err, results) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.table(results);
+                console.log('\n')
+                start();
+            }
+        })
+}
+
+
+viewDep();
